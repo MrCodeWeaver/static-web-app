6f6d686f81ac502375b430ba71493edae1352cbc
Ryan Weaver <12052413+rdweaver@users.noreply.github.com>
2022-07-07 16:03:16 -0400

    add config log

--- api/SampleFunc.cs
+++ api/SampleFunc.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.Resource;
 using System.Threading.Tasks;
@@ -12,11 +13,13 @@ namespace api
     public class SampleFunc
     {
         private readonly ILogger<SampleFunc> _logger;
+        private readonly IConfiguration _configuration;
         // The web API will only accept tokens 1) for users, and 2) having the "access_as_user" scope for this API	
         static readonly string[] scopeRequiredByApi = new string[] { "access_as_user" };
-        public SampleFunc(ILogger<SampleFunc> logger)
+        public SampleFunc(IConfiguration configuration, ILogger<SampleFunc> logger)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         [FunctionName("SampleFunc")]
@@ -24,6 +27,7 @@ namespace api
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
+            _logger.LogInformation(_configuration["AzureAd:ClientId"]);
 
             var (authenticationStatus, authenticationResponse) =
                 await req.HttpContext.AuthenticateAzureFunctionAsync();
